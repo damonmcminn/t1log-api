9992b5249b9096d4b5157f8faf778186d647d5f2
Damon McMinn <damonrmcminn@gmail.com>
2015-04-14 12:49:22 +0100

    Boilerplate
    1. Add dependencies
    2. Add comment regarding necessity to compile project if cloned from repo
    3. Ignore ES5 build dir

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+/**
+ * If you have cloned this repo, you will need to compile lib:
+ * npm run compile
+ */
+
 'use strict';
 
-const config = require('./src/config');
-const api = require('./src/api');
+const config = require('./lib/config');
+const api = require('./lib/api');
 
 api.listen(config.port, function(x) {
   console.log(x);
